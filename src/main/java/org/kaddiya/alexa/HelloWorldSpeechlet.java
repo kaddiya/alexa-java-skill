53ac3e724dee20f949c400056b5d78bf2dfbc568
kaddiya <kaddiya@gmail.com>
2017-12-27 14:24:23 +0530

    Added the handlers

--- src/main/java/org/kaddiya/alexa/HelloWorldSpeechlet.java
+++ src/main/java/org/kaddiya/alexa/HelloWorldSpeechlet.java
@@ -1,6 +1,10 @@
 package org.kaddiya.alexa;
 
+import com.amazon.speech.slu.Intent;
 import com.amazon.speech.speechlet.*;
+import com.amazon.speech.ui.PlainTextOutputSpeech;
+import com.amazon.speech.ui.Reprompt;
+import com.amazon.speech.ui.SimpleCard;
 
 
 public class HelloWorldSpeechlet implements Speechlet {
@@ -11,16 +15,57 @@ public class HelloWorldSpeechlet implements Speechlet {
 
     @Override public SpeechletResponse onLaunch(LaunchRequest request, Session session)
         throws SpeechletException {
-        return null;
+        return getWelcomeResponse();
     }
 
     @Override public SpeechletResponse onIntent(IntentRequest request, Session session)
         throws SpeechletException {
-        return null;
+        Intent intent = request.getIntent();
+        String intentName = (intent != null) ? intent.getName() : null;
+
+        if ("HelloWorldIntent".equals(intentName)) {
+            return getHelloResponse();
+        } else {
+            throw new SpeechletException("Invalid Intent");
+        }
     }
 
     @Override public void onSessionEnded(SessionEndedRequest request, Session session)
         throws SpeechletException {
 
+    }
+
+    private SpeechletResponse getHelloResponse() {
+        String speechText = "Hello world";
+
+        // Create the Simple card content.
+        SimpleCard card = new SimpleCard();
+        card.setTitle("HelloWorld");
+        card.setContent(speechText);
+
+        // Create the plain text output.
+        PlainTextOutputSpeech speech = new PlainTextOutputSpeech();
+        speech.setText(speechText);
+
+        return SpeechletResponse.newTellResponse(speech, card);
+    }
+
+    private SpeechletResponse getWelcomeResponse() {
+        String speechText = "Welcome to the Alexa Skills Kit, you can say hello";
+
+        // Create the Simple card content.
+        SimpleCard card = new SimpleCard();
+        card.setTitle("HelloWorld");
+        card.setContent(speechText);
+
+        // Create the plain text output.
+        PlainTextOutputSpeech speech = new PlainTextOutputSpeech();
+        speech.setText(speechText);
+
+        // Create reprompt
+        Reprompt reprompt = new Reprompt();
+        reprompt.setOutputSpeech(speech);
+
+        return SpeechletResponse.newAskResponse(speech, reprompt, card);
     }
 }
